32d1f00125e60a13fc80063fff6f93d588bebc07
Deep Pati <deeppati97@gmail.com>
2024-03-31 21:12:43 +0530

    added

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.querySelector("#input-box");
+const addBtn = document.querySelector(".add-btn");
+const listContainer = document.querySelector("#list-container");
+
+function addTask() {
+  if (!inputBox.value.trim()) {
+    alert("NO DATA FOUND");
+  } else {
+    let listItem = document.createElement("li");
+    listItem.textContent = inputBox.value;
+    listContainer.appendChild(listItem);
+    let span = document.createElement("span");
+    span.textContent = "X";
+    listItem.appendChild(span);
+
+    inputBox.value = "";
+  }
+
+  saveDataLocal();
+}
+
+function enterKey(event) {
+  if (event.key == "Enter") {
+    addTask();
+  }
+}
+
+function removeTask(event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("checked");
+    saveDataLocal();
+  } else if (event.target.tagName === "SPAN") {
+    event.target.parentElement.remove();
+    saveDataLocal();
+  }
+}
+
+function saveDataLocal() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function getDataLocal() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+addBtn.addEventListener("click", addTask);
+listContainer.addEventListener("click", removeTask);
+inputBox.addEventListener("keypress", enterKey);
+
+getDataLocal();
